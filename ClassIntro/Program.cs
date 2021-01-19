fe035119d5e940991a4471e4e47ce2419a08f6cd
Minayca <minaycakanberk@gmail.com>
2021-01-19 18:50:37 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
