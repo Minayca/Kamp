1bb5d48d0364896fbc7a427f402af363d8062781
Minayca <minaycakanberk@gmail.com>
2021-01-19 18:53:28 +0300

    16.01.2021

--- Donguler/Program.cs
+++ Donguler/Program.cs
@@ -6,7 +6,32 @@ namespace Donguler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç için Temel Kurs";
+            string kurs3 = "Java";
+
+            //Console.WriteLine(kurs1);
+            //Console.WriteLine(kurs2);
+            //Console.WriteLine(kurs3);
+
+            //array - dizi
+
+            string[] kurslar = new string[] {"Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya Başlangıç için Temel Kurs", "Java", "Python", "C#"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu - footer");
         }
     }
 }
