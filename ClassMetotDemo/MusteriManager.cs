6deb7198d33827f65d33749f5945dff848d2bc50
Minayca <minaycakanberk@gmail.com>
2021-01-19 19:31:33 +0300

    19.01.2021

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri "+musteri.Ad+" "+musteri.Soyad+" eklendi.");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri " + musteri.Ad + " " + musteri.Soyad + " silindi.");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri " + musteri.Ad + " " + musteri.Soyad);
+            }
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+
+            musteri1.Ad = "Beren";
+            musteri1.Soyad = "Alyüz";
+
+            musteri2.Ad = "Onur";
+            musteri2.Soyad = "Alyüz";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2};
+
+            musteriManager.Listele(musteriler);
+
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+        }
+    }
+}
