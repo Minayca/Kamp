47ad68adeeaef976539961a4ae9d75f0207ece02
Minayca <minaycakanberk@gmail.com>
2021-01-23 13:47:14 +0300

    20.01.2021

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic; //Listeler için ekliyoruz.
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler.Add("İlker");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+        }
+    }
+}
