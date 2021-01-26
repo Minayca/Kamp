4b562ce0e106fb971934e16e7e084e6d11927653
Minayca <minaycakanberk@gmail.com>
2021-01-26 20:20:47 +0300

    26.01.2021 #Müşteri kimlik kontrolü

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,38 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            return TaskAsync(customer).Result;
+        }
+
+        public static async Task<bool> TaskAsync(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap12);
+            var durum = await (client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId), customer.FirstName.ToUpper(),
+                customer.LastName.ToUpper(), customer.DateOfBirth.Year));
+            return durum.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+    //public class MernisServiceAdapter : ICustomerCheckService
+
+    //{
+    //    public bool CheckIfRealPersonAsync(Customer customer)
+    //    {
+    //        KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap12);
+    //        return await client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId),
+    //            customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year);
+
+    //    }
+    //}
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer {DateOfBirth = new DateTime(1985,1,6),
+                FirstName = "Engin", LastName = "Demiroğ", NationalityId = "28861499108"});
+
+        }
+    }
+}
